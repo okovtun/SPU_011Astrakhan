7b98a0a0fba9250dd9eeb3d37cf946d7810b680c
Clayman <Clayman@DESKTOP-DM8E77U>
2021-03-06 10:13:21 +0200

    ReadMe

---
+++ Functions/Functions/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int price_of_coffee;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+	{
+		int a = 2;
+		cout << a << endl;
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+https://github.com/okovtun/SPU_011Astrakhan
+https://www.youtube.com/watch?v=ZWOIH3LTdYo&list=PLeqyOOqxeiIN8vf9jj59FxEoXWLeBUHuh&index=2
