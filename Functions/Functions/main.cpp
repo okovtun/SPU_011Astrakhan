65949cf5f65683da52822fc76e036c5385101601
Clayman <Clayman@DESKTOP-DM8E77U>
2021-03-06 11:32:04 +0200

    Functions day 1

--- Functions/Functions/main.cpp
+++ Functions/Functions/main.cpp
@@ -1,17 +1,59 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
+
+void FillRand(int arr[], const int n);	//Прототип функции, Объявление функции - Function declaration
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	int price_of_coffee;
-	for (int i = 0; i < 5; i++)
+	const int ARR_SIZE = 5;
+	int arr[ARR_SIZE];
+	FillRand(arr, ARR_SIZE);	//Function call - вызов, или использование функции
+								//Один раз написанная функция может быть вызвана сколько угодно раз
+	Print(arr, ARR_SIZE);
+	Sort(arr, ARR_SIZE);
+	Print(arr, ARR_SIZE);
+	////////////////////////////////////////////////////////////
+	const int BRR_SIZE = 8;
+	int brr[BRR_SIZE];
+	FillRand(brr, BRR_SIZE);
+	Print(brr, BRR_SIZE);
+	Sort(brr, BRR_SIZE);
+	Print(brr, BRR_SIZE);
+}
+
+void FillRand(int arr[], const int n)//Реализация функции, определение функции - Function definition
+{
+	//Заполняем массив случайными числами:
+	for (int i = 0; i < n; i++)
 	{
-		cout << i << "\t";
+		arr[i] = rand();
+	}
+}
+void Print(int arr[], const int n)
+{
+	//Выводим массив на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	//Сортировка массива: 
+	for (int i = 0; i < n; i++)
 	{
-		int a = 2;
-		cout << a << endl;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
 	}
 }
